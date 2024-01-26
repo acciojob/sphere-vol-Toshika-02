622a98e1bca968dd3f34400e5538e7e313ba55be
Toshika-02 <105552050+Toshika-02@users.noreply.github.com>
2024-01-26 23:03:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
 function volume_sphere() {
     //Write your code here
-	// const form = document.getElementById('MyForm');
-	// let r = Number(form.radius.value)
-	// form.volume.value = (4*Math.PI*(r**3) / 3);
-	// return false;
-	 var radius = parseFloat(document.getElementById('radius').value);
-    if (!isNaN(radius)) {
-    var volume = (4/3) * Math.PI * Math.pow(radius, 3);
-    document.getElementById('volume').value = volume.toFixed(2);
-  } 
-  
-  return false;
+	const form = document.getElementById('MyForm');
+	let r = Number(form.radius.value)
+	form.volume.value = (4*Math.PI*(r**3) / 3);
+	return false;
+	 
   
 } 
 
