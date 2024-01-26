99b152d7ac555708eb51da4780b83c807204a679
Toshika-02 <105552050+Toshika-02@users.noreply.github.com>
2024-01-26 19:13:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function volume_sphere() {
     //Write your code here
+	const radius = parseFloat(document.getElementById('radius').value);
+	if(isNaN(radius) || radius <= 0){
+		//alert("Please enter a valid number");
+		return;
+	}
+	 const valume = (4/3) * Math.PI * Math.pow(radius, 3);
+	document.getElementById('valume').value = valume.toFixed(2);
+	return false;
   
 } 
 
